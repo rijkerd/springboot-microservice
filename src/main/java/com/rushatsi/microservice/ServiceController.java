d99340b5b1b0d49a5ba9156504dfac5a11d330ad
Richard Elianami <richard.elianami@equitybank.co.tz>
2023-10-01 12:47:52 +0300

    initial-bootstrap

---
+++ src/main/java/com/rushatsi/microservice/ServiceController.java
@@ -0,0 +1,14 @@
+package com.rushatsi.microservice;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello! No Dockerfile";
+    }
+}
